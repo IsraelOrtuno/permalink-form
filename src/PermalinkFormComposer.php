568bc8e9f380ddca3229f6439e602cca644c93ec
Israel Ortuño <ai.ortuno@gmail.com>
2018-06-11 16:26:23 +0200

    Added comments and refactoring

--- src/PermalinkFormComposer.php
+++ src/PermalinkFormComposer.php
@@ -8,20 +8,42 @@ use Illuminate\Contracts\View\View;
 
 class PermalinkFormComposer
 {
+    /**
+     * Run the composer.
+     *
+     * @param View $view
+     */
     public function compose(View $view)
     {
         $route = request()->route();
 
-        $permalink = $this->getPermalinkFromView($view);
+        $permalink = $this->guessPermalinkFromView($view);
 
         $view->with([
             'permalink'     => $view['permalink'] ?? $permalink ?? [],
-            'permalinkPath' => implode('/', Permalink::parentPath($permalink)),
+            'permalinkPath' => $this->getPermalinkPath($permalink),
             'permalinkData' => array_merge($route->parameters, $view['permalinkData'] ?? [])
         ]);
     }
 
-    protected function getPermalinkFromView($view)
+    /**
+     * Get the heritage for the given permalink
+     *
+     * @param null $permalink
+     * @return array|string
+     */
+    protected function getPermalinkPath($permalink = null)
+    {
+        return $permalink ? implode('/', Permalink::parentPath($permalink)) : [];
+    }
+
+    /**
+     * Guess the main permalinkable entity from the view.
+     *
+     * @param $view
+     * @return null
+     */
+    protected function guessPermalinkFromView($view)
     {
         foreach ($view->getData() as $variable) {
             if (is_object($variable) && in_array(HasPermalinks::class, class_uses_recursive($variable))) {
