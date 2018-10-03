ceb0021303179386da7e098b220236ebcd87838c
Israel Ortuño <ai.ortuno@gmail.com>
2018-10-03 13:44:18 +0200

    Updated title and description for base options

--- resources/views/components/meta.blade.php
+++ resources/views/components/meta.blade.php
@@ -14,7 +14,7 @@
         <!-- Page title -->
         <div class="form-group">
             <label>Page title <span class="small">@{{ title.length }}/60</span></label>
-            <input autocomplete="off" type="text" class="form-control" name="permalink[seo][meta][title]" v-model="title">
+            <input autocomplete="off" type="text" class="form-control" name="permalink[seo][title]" v-model="title">
             <div class="text-danger font-weight-bold small mt-2" v-if="title.length > 60">
                 The title should not be longer than 60 characters.
             </div>
@@ -23,7 +23,7 @@
         <!-- Meta description -->
         <div class="form-group">
             <label>Meta description <span class="small">@{{ description.length }}/300</span></label>
-            <textarea class="form-control" name="permalink[seo][meta][description]" rows="3" v-model="description"></textarea>
+            <textarea class="form-control" name="permalink[seo][description]" rows="3" v-model="description"></textarea>
             <div class="text-danger font-weight-bold small mt-2" v-if="description.length > 300">
                 The description should not be longer than 300 characters.
             </div>
